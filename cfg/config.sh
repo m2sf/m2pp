bf12417866ecb6fd64d5f299647e560d22d8ce7a
Benjamin Kowarsch <trijezdci@users.noreply.github.com>
2017-07-20 23:24:01 +0900

    work in progress

--- cfg/config.sh
+++ cfg/config.sh
@@ -2,29 +2,20 @@
 echo "*** M2PP build configuration script for Unix/POSIX ***"
 #
 # ---------------------------------------------------------------------------
-# compiler menu
+# dialect menu
 # ---------------------------------------------------------------------------
 echo ""
-echo "Compiler Selection"
-PS3="Modula-2 compiler: "
-select compiler in \
-  "GNU Modula-2" "p1 Modula-2" "XDS Modula-2" "generic PIM compiler" Quit
+echo "Dialect Selection"
+PS3="Modula-2 dialect: "
+select dialect in "ISO Modula-2" "PIM Modula-2" Quit
 do
-  case $compiler in
-    "GNU Modula-2")
-      compilerID="gm2"
+  case $dialect in
+    "ISO Modula-2")
+      dialectID="iso"
       break
       ;;
-    "p1 Modula-2")
-      compilerID="p1"
-      break
-      ;;
-    "XDS Modula-2")
-      compilerID="xds"
-      break
-      ;;
-    "generic PIM compiler")
-      compilerID="pim"
+    "PIM Modula-2")
+      dialectID="pim"
       break
       ;;
     Quit)
@@ -34,31 +25,30 @@ do
 done
 #
 # ---------------------------------------------------------------------------
-# io-library menu
+# compiler menu
 # ---------------------------------------------------------------------------
 echo ""
-echo "I/O Library Selection"
-PS3="I/O library: "
+echo "Compiler Selection"
+PS3="Modula-2 compiler: "
 #
 # ---------------------------------------------------------------------------
-# GM2 io-library selection
+# ISO compiler selection
 # ---------------------------------------------------------------------------
-if [ "$compilerID" = "gm2" ]
+if [ "$dialectID" = "iso" ]
 then
-  select iolib in \
-    "POSIX I/O library" "ISO I/O library" "PIM I/O library" Quit
+  select compiler in "GNU Modula-2" "p1 Modula-2" "XDS Modula-2" Quit
   do
-    case $iolib in
-      "POSIX I/O library")
-        iolibID="posix"
+    case $compiler in
+      "GNU Modula-2")
+        compilerID="gm2"
         break
         ;;
-      "ISO I/O library")
-        iolibID="iso"
+      "p1 Modula-2")
+        compilerID="p1"
         break
         ;;
-      "PIM I/O library")
-        iolibID="pim"
+      "XDS Modula-2")
+        compilerID="xds"
         break
         ;;
       Quit)
@@ -68,12 +58,41 @@ then
   done
 #
 # ---------------------------------------------------------------------------
-# p1 and XDS io-library selection
+# PIM compiler selection
 # ---------------------------------------------------------------------------
-elif [ "$compilerID" = "p1" ] || [ "$compilerID" = "xds" ]
+elif [ "$dialectID" = "pim" ]
 then
-  select iolib in \
-    "POSIX" "ISO" Quit
+  select compiler in "GNU Modula-2" "generic PIM compiler" Quit
+  do
+    case $compiler in
+      "GNU Modula-2")
+        compilerID="gm2"
+        break
+        ;;
+      "generic PIM compiler")
+        compilerID="pim"
+        break
+        ;;
+      Quit)
+        exit
+        ;;
+    esac
+  done
+fi
+#
+# ---------------------------------------------------------------------------
+# io-library menu
+# ---------------------------------------------------------------------------
+echo ""
+echo "I/O Library Selection"
+PS3="I/O library: "
+#
+# ---------------------------------------------------------------------------
+# ISO io-library selection
+# ---------------------------------------------------------------------------
+if [ "$dialectID" = "iso" ]
+then
+  select iolib in "POSIX I/O library" "ISO I/O library" Quit
   do
     case $iolib in
       "POSIX I/O library")
@@ -93,8 +112,24 @@ then
 # ---------------------------------------------------------------------------
 # PIM io-library selection
 # ---------------------------------------------------------------------------
-else
-  iolibID="pim"
+elif [ "$dialectID" = "pim" ]
+then
+  select iolib in "POSIX I/O library" "PIM I/O library" Quit
+  do
+    case $iolib in
+      "POSIX I/O library")
+        iolibID="posix"
+        break
+        ;;
+      "PIM I/O library")
+        iolibID="pim"
+        break
+        ;;
+      Quit)
+        exit
+        ;;
+    esac
+  done
 fi
 #
 # ---------------------------------------------------------------------------
@@ -134,14 +169,142 @@ do
 done
 #
 # ---------------------------------------------------------------------------
-# summary
+# m2pp src path
+# ---------------------------------------------------------------------------
+echo ""
+read -rp "Path of M2PP src directory: " srcpath
+# check path for leading ~, expand if present
+if [ "${srcpath: 1}" != "~" ]
+then
+  srcpath="$HOME${srcpath:1}"
+fi
+#
+# check path for final /, add one if missing
+if [ "${srcpath:(-1)}" != "/" ]
+then
+  srcpath="$srcpath/"
+fi
+#
+# check if directory at path exists
+if [ ! -d "$srcpath" ]
+then
+  echo "directory $srcpath does not exist"
+  exit
+fi
+#
+# ---------------------------------------------------------------------------
+# print summary
 # ---------------------------------------------------------------------------
 echo ""
 echo "Selected build configuration"
-echo "Compiler     : $compiler ($compilerID)"
-echo "I/O library  : $iolib ($iolibID)"
-echo "Memory model : $mm ($hashlibID)"
+echo "Dialect       : $dialect ($dialectID)"
+echo "Compiler      : $compiler ($compilerID)"
+echo "I/O library   : $iolib ($iolibID)"
+echo "Memory model  : $mm ($hashlibID)"
+echo "M2PP src path : $srcpath"
+#
+# ---------------------------------------------------------------------------
+# user confirmation
+# ---------------------------------------------------------------------------
+echo ""
+read -rp "Are these details correct? (y/n) : " confirm
+if [ "$confirm" != "y" ] && [ "$confirm" != "Y" ]
+then
+  exit
+fi
+#
+# ---------------------------------------------------------------------------
+# copy source files
+# ---------------------------------------------------------------------------
+echo ""
+echo Copying source files corresponding to selected build configuration ...
+echo ""
 
-# TO DO #
+# module Hash
+echo copying "${srcpath}Hash.${hashlibID}.def"
+echo  to "${srcpath}Hash.def"
+#cp "${srcpath}Hash.${hashlibID}.def" "${srcpath}Hash.def"
+echo copying "${srcpath}imp/Hash.${hashlibID}.mod"
+echo  to "${srcpath}imp/Hash.mod"
+#cp "${srcpath}imp/Hash.${hashlibID}.mod" "${srcpath}/imp/Hash.mod"
+
+# module Infile
+echo copying "${srcpath}Infile.${dialectID}.def"
+echo  to "${srcpath}Infile.def"
+#cp "${srcpath}Infile.${dialectID}.def" "${srcpath}Infile.def"
+
+# module Outfile
+echo copying "${srcpath}Outfile.${dialectID}.def"
+echo  to "${srcpath}Outfile.def"
+#cp "${srcpath}Outfile.${dialectID}.def" "${srcpath}Outfile.def"
+
+# module String
+echo copying "${srcpath}String.${dialectID}.def"
+echo  to "${srcpath}String.def"
+#cp "${srcpath}String.${dialectID}.def" "${srcpath}String.def"
+echo copying "${srcpath}imp/String.${dialectID}.mod"
+echo  to "${srcpath}imp/String.mod"
+#cp "${srcpath}imp/String.${dialectID}.def" "${srcpath}imp/String.mod"
+
+# module Terminal
+if [ "$dialectID" = "iso" ]
+then
+  echo copying "${srcpath}Terminal.iso.def"
+  echo  to "${srcpath}Terminal.def"
+#cp "${srcpath}Terminal.iso.def" "${srcpath}Terminal.def"
+  echo copying "${srcpath}imp/Terminal.iso.mod"
+  echo  to "${srcpath}imp/Terminal.mod"
+#cp "${srcpath}imp/Terminal.iso.mod" "${srcpath}imp/Terminal.mod"
+else
+  if [ -f "${srcpath}Terminal.def" ]
+  then
+    echo removing "${srcpath}Terminal.def"
+    # rm "${srcpath}Terminal.def"
+  fi
+  if [ -f "${srcpath}imp/Terminal.mod" ]
+  then
+    echo removing "${srcpath}imp/Terminal.mod"
+    # rm "${srcpath}imp/Terminal.mod"
+  fi
+fi
+
+# module BasicFileIO
+echo copying "${srcpath}imp/BasicFileIO/BasicFileIO.${iolibID}.mod"
+echo  to "${srcpath}imp/BasicFileIO.mod"
+#cp "${srcpath}imp/BasicFileIO/BasicFileIO.${iolibID}.mod"
+#\  "${srcpath}imp/BasicFileIO.mod"
+
+# module FileSystemAdapter
+if [ "$iolibID" = "iso" ]
+then
+  echo copying \
+    "${srcpath}imp/FileSystemAdapter/FileSystemAdapter.${compilerID}.mod"
+  echo  to "${srcpath}imp/FileSystemAdapter.mod"
+#cp "${srcpath}imp/BasicFileIO/BasicFileIO.${iolibID}.mod"
+#\  "${srcpath}imp/BasicFileIO.mod"
+else
+  echo copying \
+    "${srcpath}imp/FileSystemAdapter/FileSystemAdapter.${iolibID}.mod"
+  echo  to "${srcpath}imp/FileSystemAdapter.mod"
+#cp "${srcpath}imp/BasicFileIO/BasicFileIO.${iolibID}.mod"
+#\  "${srcpath}imp/BasicFileIO.mod"
+fi
+
+# foreign module stdio
+if [ "$iolibID" = "posix" ] || [ "$compilerID" = "p1" ]
+then
+  echo copying "${srcpath}posix/stdio.${compilerID}.def"
+  echo  to "${srcpath}stdio.def"
+#cp "${srcpath}posix/stdio.${compilerID}.def" "${srcpath}/stdio.def"
+else
+  if [ -f "${srcpath}stdio.def" ]
+  then
+    echo removing "${srcpath}stdio.def"
+    # rm "${srcpath}stdio.def"
+  fi
+fi
+
+echo ""
+echo "Build configuration completed."
 
 # END OF FILE
